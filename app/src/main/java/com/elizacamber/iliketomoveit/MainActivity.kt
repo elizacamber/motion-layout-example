3e19dd167c2a1843da92a1fa57622e28b8928785
elizacamber <elizacamber@gmail.com>
2018-09-26 00:22:47 +0200

    fix and use the recyclerview. Delete the cardview

--- app/src/main/java/com/elizacamber/iliketomoveit/MainActivity.kt
+++ app/src/main/java/com/elizacamber/iliketomoveit/MainActivity.kt
@@ -7,34 +7,32 @@ import kotlinx.android.synthetic.main.activity_main_start.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val contacts = arrayListOf<Contact>()
+    private val NUMBER_OF_ITEMS = 8
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_start)
 
         getData()
 
-//        rv_contacts.apply {
-//            setHasFixedSize(true)
-//            layoutManager = LinearLayoutManager(context)
-//            adapter = ContactsAdapter(context, contacts)
-//        }
+        rv_contacts.apply {
+            setHasFixedSize(true)
+            layoutManager = LinearLayoutManager(context)
+            adapter = ContactsAdapter(context, contacts)
+        }
     }
 
     private fun getData(){
         var count =0
         while (count < NUMBER_OF_ITEMS) {
-            contacts.add(Contact(getDrawableForPosition(count), "XYZ", "645654654"))
+            contacts.add(Contact(getDrawableForPosition(count+1), "XYZ", "645654654"))
             count ++
         }
     }
 
-    fun getDrawableForPosition(position:Int): Int? {
+    private fun getDrawableForPosition(position:Int): Int? {
         val name = "avatar_$position"
         return resources.getIdentifier(name, "drawable",packageName)
-    }
-
-    companion object {
-        val contacts = arrayListOf<Contact>()
-        const val NUMBER_OF_ITEMS = 8
     }
 }
