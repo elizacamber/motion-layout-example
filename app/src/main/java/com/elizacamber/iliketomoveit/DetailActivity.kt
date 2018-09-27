555a1d79c13a16c06a2f517997de0aaa062a359e
elizacamber <elizacamber@gmail.com>
2018-09-27 18:52:27 +0200

    fab button icon morphing from pencil to checkmark and vice versa

--- app/src/main/java/com/elizacamber/iliketomoveit/DetailActivity.kt
+++ app/src/main/java/com/elizacamber/iliketomoveit/DetailActivity.kt
@@ -2,19 +2,43 @@ package com.elizacamber.iliketomoveit
 
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.graphics.drawable.Animatable
+import android.graphics.drawable.AnimatedVectorDrawable
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.app.AppCompatDelegate
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_detail.*
 
 class DetailActivity : AppCompatActivity() {
 
+    var fabFlag = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
 
         Glide.with(this).load(intent.getIntExtra(INTENT_CONTACT_AVATAR, 0)).into(iv_avatar)
         tv_name.text = intent.getStringExtra(INTENT_CONTACT_NAME)
+
+        val animDrawable = getDrawable(R.drawable.avd_edit_done) as AnimatedVectorDrawable
+        val animDrawable2 = getDrawable(R.drawable.avd_done_edit) as AnimatedVectorDrawable
+
+        fab.setImageDrawable(animDrawable)
+
+        fab.setOnClickListener {
+            if (fabFlag) {
+                // turn into done
+                (fab.drawable as Animatable).start()
+                fab.setImageDrawable(animDrawable)
+            } else {
+                // turn into pencil
+                (fab.drawable as Animatable).start()
+                fab.setImageDrawable(animDrawable2)
+            }
+            fabFlag = !fabFlag
+        }
     }
 
     companion object {
